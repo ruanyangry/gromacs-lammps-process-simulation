b601a136c0b969b4e2c1f81ef0065801269f378b
Ruan Yang <ruanyang_njut@163.com>
2018-06-10 08:57:32 +0800

    Add files via upload

--- spidersmiles.py
+++ spidersmiles.py
@@ -4,17 +4,22 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 import numpy as np
+from time import sleep
 
 registrynumber=np.loadtxt("casnumber.txt")
 
 simle={}
 
 for i in range(len(registrynumber)):
-	html=urlopen("http://www.chemspider.com/Chemical-Structure.%d.html"%(registrynumber[i]))
-	bsObj=BeautifulSoup(html,"html.parser")
-	bsObj = bsObj.findAll("button",id = 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_moreDetails_copySmiles')
-	smilename=re.findall(r"this,(.*?), 'SMILES'", bsObj[0]["onmouseover"])
-	simle[registrynumber[i]]=smilename
+	try:
+		html=urlopen("http://www.chemspider.com/Chemical-Structure.%d.html"%(registrynumber[i]))
+		bsObj=BeautifulSoup(html,"html.parser")
+		bsObj = bsObj.findAll("button",id = 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_moreDetails_copySmiles')
+		smilename=re.findall(r"this,(.*?), 'SMILES'", bsObj[0]["onmouseover"])
+		simle[registrynumber[i]]=smilename
+	except:
+		print("%d not get from chemspider"%(registrynumber[i]))
+	sleep(5)
 	
 with open("molsimles.txt","w") as f:
 	for key,value in simle.items():
